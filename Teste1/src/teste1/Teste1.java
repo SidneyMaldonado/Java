9be5a57c7ebefd595010cef2b4121481afa508fd
sidneymaldonado <sidneymaldonado@gmail.com>
2017-06-28 10:54:53 -0400

    mais um teste

--- Teste1/src/teste1/Teste1.java
+++ Teste1/src/teste1/Teste1.java
@@ -16,7 +16,7 @@ public class Teste1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Oi mais uma vez Linux...");
+        System.out.println("Oi mais uma vez Linux ......");
     }
     
 }
